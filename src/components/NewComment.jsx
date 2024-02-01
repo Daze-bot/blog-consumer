073083b721b42aab3d675262a7a91090c6078fea
Code4Daze <jevon.fillette@gmail.com>
2024-01-31 21:57:09 -0500

    Fix comment submission

--- src/components/NewComment.jsx
+++ src/components/NewComment.jsx
@@ -1,4 +1,5 @@
 import { useState } from "react";
+import { Navigate } from "react-router-dom";
 
 const NewComment = ({postID}) => {
   const [data, setData] = useState({
@@ -6,6 +7,8 @@ const NewComment = ({postID}) => {
     commentText: "",
     postID: postID
   });
+
+  const [submitted, setSubmitted] = useState(false);
 
   const handleSubmit = () => {
     fetch(`http://localhost:3000/posts/${postID}/comments`, {
@@ -15,7 +18,10 @@ const NewComment = ({postID}) => {
       },
       body: JSON.stringify(data)
     })
-      .then(res => res.json());
+      .then((res) => {
+        setSubmitted(true);
+        return res.json();
+      });
   };
 
   const handleChange = (event) => {
@@ -30,35 +36,38 @@ const NewComment = ({postID}) => {
   }
 
   return (
-    <div className="new-comment">
-      <h3>Add New Comment</h3>
-      <form onSubmit={handleSubmit}>
-        <label htmlFor="commentName">Name</label>
-        <input
-          required
-          name="commentName"
-          id="commentName"
-          type="text"
-          value={data.commentName}
-          onChange={handleChange}
-          maxLength="25"
-        ></input>
-
-        <label htmlFor="commentText">Comment</label>
-        <textarea
-          required
-          name="commentText"
-          id="commentText"
-          rows="5"
-          maxLength="1400"
-          value={data.commentText}
-          onChange={handleChange}
-          placeholder="Type your comment here"
-        >
-        </textarea>
-
-        <button type="submit">Publish</button>
-      </form>
+    <div>
+      {submitted &&
+        <Navigate to={`/posts/${postID}`}/>
+      }
+      <div className="new-comment">
+        <h3>Add New Comment</h3>
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="commentName">Name</label>
+          <input
+            required
+            name="commentName"
+            id="commentName"
+            type="text"
+            value={data.commentName}
+            onChange={handleChange}
+            maxLength="25"
+          ></input>
+          <label htmlFor="commentText">Comment</label>
+          <textarea
+            required
+            name="commentText"
+            id="commentText"
+            rows="5"
+            maxLength="1400"
+            value={data.commentText}
+            onChange={handleChange}
+            placeholder="Type your comment here"
+          >
+          </textarea>
+          <button type="submit">Publish</button>
+        </form>
+      </div>
     </div>
   )
 };
